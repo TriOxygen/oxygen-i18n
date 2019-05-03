45d628c533d086df0827ead82606186045f3ec17
Oz <oz@gur.space>
2019-05-03 14:23:00 +0200

    v3.3.4

--- src/buildMessage.js
+++ src/buildMessage.js
@@ -1,5 +1,5 @@
 //@flow
-import type { Token } from 'createTranslator';
+import type { Token } from "./createTranslator";
 export default (str: string, ...values: Array<Token>) => {
   return str.replace(/{(\d)}/g, (_, index) => values[Number(index)].toLocaleString());
 };
