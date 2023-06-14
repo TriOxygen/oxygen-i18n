1472e79ed04f1c85b43c8d18b07ca1f12e363d8a
Öz <oz@gur.space>
2023-06-14 14:49:33 +0200

    Add better date options

--- src/formatters/createNumberFormatter.ts
+++ src/formatters/createNumberFormatter.ts
@@ -1,12 +1,12 @@
 import type { NumberOptions, Token } from '../createTranslator';
 const numberStyleDecimal = 'decimal';
 
-const createNumberFormatter = (locale: string, options: NumberOptions = {}) => (v: Token, _format?: string) => {
+const createNumberFormatter = (locale: string, options: NumberOptions = {}) => (v: Token, maximumFractionDigits?: string) => {
   if (typeof v === 'number') {
     const numberOptions = {
       style: numberStyleDecimal,
       minimumFractionDigits: 0,
-      maximumFractionDigits: 3,
+      maximumFractionDigits: maximumFractionDigits ? parseInt(maximumFractionDigits, 10) : 2,
       ...options
     };
 
